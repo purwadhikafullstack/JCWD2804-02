5bf8f729fed4bcf836a30c590c8dd754636f13c2
niaskn <sitikurnia.nurzannah@gmail.com>
2024-08-16 10:34:00 +0700

    build: location api

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -12,6 +12,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import storeRouter from '../src/routers/storeRouters.ts'
+import locationRouter from '../src/routers/locationRouter.ts'
 
 dotenv.config({
   path: path.resolve(process.cwd(), '.env.development')
@@ -28,6 +29,7 @@ app.use(
 
 app.use(express.json());
 app.use("/api", storeRouter);
+app.use("/api", locationRouter);
 
 app.listen(PORT, () => {
   console.log("Listening on port: ", PORT);
